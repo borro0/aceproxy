0bb9322e507790d3fc6010cfee1e917db7a0f2fd
Gijs <gijsmw@hotmail.com>
2018-06-01 12:33:20 +0200

    First commit of writing data to file

---
+++ csvwriter.py
@@ -0,0 +1,26 @@
+import time
+from multiprocessing import Event
+from queue import Empty
+
+stop_command = Event()
+
+
+class CSVWriter(object):
+
+    def __init__(self, filename):
+        # Append current date and time to filename
+        self.filename = filename + time.strftime("%Y%m%d-%H%M%S")
+
+    def writer(self, queue):
+        with open(self.filename, 'w+') as f:
+            while not queue.empty() or not stop_command.is_set():
+                try:
+                    data = queue.get_nowait()
+                    # Floating point of time since epoch in seconds
+                    ts = time.time()
+                    f.write("{0}|{1},".format(str(data), ts))
+
+                except Empty:
+                    # This should never happen
+                    pass
+
