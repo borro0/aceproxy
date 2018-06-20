8717d98b594c5f5411699fa6214cbd83e5f79c28
Unknown <borisblokland@gmail.com>
2018-06-20 18:27:32 +0200

    Increase delay

--- scripts2.sh
+++ scripts2.sh
@@ -4,6 +4,6 @@ hostname=$(hostname)
 
 ../start-engine --client-console & python acehttp.py & bwm-ng -o csv > $hostname.csv &
 
-sleep 10
+sleep 15
 
 curl --output /dev/null http://127.0.0.1:8000/pid/0bdf6de85fa5f2473d6a47210c598161d362d201/stream.mp4
