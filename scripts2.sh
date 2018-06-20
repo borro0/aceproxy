a497b750e81ea5e57a921eb327f44e4f82d22d29
Unknown <borisblokland@gmail.com>
2018-06-20 11:18:39 +0200

    Changed CiD

--- scripts2.sh
+++ scripts2.sh
@@ -4,4 +4,4 @@
 
 sleep 10
 
-curl --output /dev/null http://127.0.0.1:8000/pid/31ed3696c78bf651c25a8759803c6345912a2a9e/stream.mp4
+curl --output /dev/null http://127.0.0.1:8000/pid/0bdf6de85fa5f2473d6a47210c598161d362d201/stream.mp4
